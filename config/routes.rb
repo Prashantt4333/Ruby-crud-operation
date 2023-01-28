accced35dd5e3eafc9e81ad759bdd1edd15aab63
prashantt4333@gmail.com <prashantt4333@gmail.com>
2023-01-28 17:05:12 +0530

    Ruby crud operation

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  #get 'students/index'
+  #get 'students/show'
+  #get 'students/edit'
+  #get 'students/newe'
+  resources :students
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+Student.create(name:"Prashant",age:"21",)
+Student.create(name:"Yogesh",age:"25",)
+Student.create(name:"Rohit",age:"51",)
+Student.create(name:"Pankaj",age:"12",)
+Student.create(name:"Aakash",age:"05",)
